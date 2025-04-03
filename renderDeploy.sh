616aa970530bf814ee26bc8ca23f10faa263fb20
enrique.andujar <enrique.andujar@estudiantat.upc.edu>
2025-04-03 15:06:47 +0200

    Hopefully fixed

--- renderDeploy.sh
+++ renderDeploy.sh
@@ -1,3 +1,10 @@
-sudo docker compose --env-file ./Mybits2/.env build
-sudo docker compose --env-file ./Mybits2/.env up -d db
-sudo docker compose --env-file ./Mybits2/.env up web
+#!/usr/bin/env bash
+
+# Build the Docker images
+docker compose --env-file ./Mybits2/.env build
+
+# Start the database service
+docker compose --env-file ./Mybits2/.env up -d db
+
+# Start the web service
+docker compose --env-file ./Mybits2/.env up web
