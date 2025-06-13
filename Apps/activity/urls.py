3fa3cbbfa427e9e9445a0a96d75af32ae1610250
Enrique Andujar <117351843+enric4000@users.noreply.github.com>
2025-06-14 01:39:38 +0200

    Revert "Feature/hardware"

--- Apps/activity/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("create/", views.ActivityCreateView.as_view(), name="activity-create"),
-    path("<uuid:pk>/checkin/", views.ActivityCheckinView.as_view(), name="activity-join"),
-    path("<uuid:pk>/", views.ActivityCRUDView.as_view(), name="activity-detail"),
-    path("", views.ActivityView.as_view(), name="activity-list"),
-]
